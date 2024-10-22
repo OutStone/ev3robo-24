b7c6f1eb24c57a9b2428536d049415b80817d9f5
OutStone <dauxid@gmail.com>
2024-10-22 17:48:15 +0200

    Added basic wall following

--- UltraSonic.py
+++ UltraSonic.py
@@ -20,6 +20,7 @@ Buttons = {
     'front' : 'S1'
 }
 ColorSensor_port = 'S2'
+USSensor_port = 'S2'
 
 Wheel_Diameter = 0 # TODO: right value
 Axle_Track = 0 # TODO: right value
@@ -38,6 +39,7 @@ DriveBase = DriveBase( LeftMotor, RightMotor, Wheel_Diameter, Axle_Track )
 
 # Sensors
 ColorSensor = ColorSensor( ColorSensor_port )
+USSensor = UltrasonicSensor( USSensor_port )
 
 FrontBtn =  TouchSensor( Buttons['front'] )
 
@@ -58,14 +60,21 @@ def color(inputColor):
 ##--##--##--## CODE CONSTANTS ##--##--##--##
 DriveSpeed = 100 # in milemetrs per second
 
+WallDistance = 100 # in milimeters from the sensor not the centre of the robot!
+        # TODO: the distances will differ in value based on the drive line we are in
+koeficient = 1 # TODO: find the right value
+
+DistanceAvrg = WallDistance
+ValuesInAvrg = 10
+
+
+
 ##--##--##--## GAME LOOP ##--##--##--##
 ev3.speaker.beep()
 
 turn_count = 0
 
 while RUN:
-    DriveBase.drive()
-
     # color detection
     colorDetected = ColorSensor.color()
     if colorDetected != None:
@@ -81,3 +90,9 @@ while RUN:
         # break the program
         if turn_count == 2:
             break
+
+    # Following wall with ultrasonic sensor
+    dist = USSensor.distance(silent=False)
+    error = WallDistance - dist
+
+    DriveBase.drive(DriveSpeed, error * koeficient)
